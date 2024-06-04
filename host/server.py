0a88542c0cf306861505bd8b8ef9917161398ff0
Donutto <phutadol5627@gmail.com>
2024-06-04 15:46:59 +0700

    update SERVER is CCTV bkk

--- host/server.py
+++ host/server.py
@@ -1,2 +1,3 @@
 #SERVER = "http://10.200.0.42:30800"  #CCTV1
-SERVER = "http://cctv.naimueang.com:30800"  #CCTV2
+# SERVER = "http://cctv.naimueang.com:30800"  #CCTV2
+SERVER = "https://bkk.larry-cctv.com"  #CCTV bkk
