5ebc41b847136de77d34037756387d0f8b103941
Donutto <phutadol5627@gmail.com>
2024-06-05 13:47:55 +0700

    change name file

---
+++ 4_live.py
@@ -0,0 +1,52 @@
+import requests
+import pandas as pd
+import json
+from host.server import SERVER
+from host.api import API
+import os
+
+
+# dir_path = r'C:\Users\phuta\Desktop\Playground_UploadData-main\CSV - larry1\CCTV-out-dt'  #for edit at home
+
+dir_path = r'C:\Users\phutadon\OneDrive\Desktop\Playground_UploadData-main\CSV - larry1\CCTV-out-dt'
+
+
+for path in os.listdir(dir_path):  #path
+    if os.path.isfile(os.path.join(dir_path, path)):
+        print(os.path.join(dir_path, path))
+        data = pd.read_csv( os.path.join(dir_path, path)) 
+
+        url = f"{SERVER}/core/api/streaming/v1.1/Observations"
+
+        headers = {
+        'Content-Type': 'application/json',
+        'API-Key':API
+        }
+            
+        for i in range (len(data)):
+            datastream = data._get_value(i,'DATASTREAM_ID (Live)')
+            name = data._get_value(i,'CAMERA_NAME')
+            # pole_name = data._get_value(i,'POLE_NAME')
+            # description = data._get_value(i,'POLE_DESCRIPTION')
+            # port = data._get_value(i,'RTC_PORT')
+            # ip = data._get_value(i,'RTC_IP')
+            # lon = data._get_value(i,'LON')
+            # lat = data._get_value(i,'LAT')
+            
+            # link = f"http://{ip}:{port}/api/stream.m3u8?src={name}&mp4=flac" ## create link for play in vlc
+            link = f"https://rtc-bkk-bma-ba-2-165.larry-cctv.com/api/stream.mp4?src={name}&mp4=flac" ## create link for play in vlc
+            
+            print(i,link)
+            
+            payload = json.dumps({
+            "result" : link,
+            "resultType": "string",
+            "Datastream":{"@iot.id":datastream}
+            })
+            # print(datastream,name)
+            response = requests.request("POST", url, headers=headers, data=payload)
+            
+            print(response)
+            print (link)
+
+
---
+++ 5_create_link.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+
+# ระบุไดเรกทอรีที่เก็บไฟล์ CSV
+dir_path = r'C:\Users\phutadon\OneDrive\Desktop\Playground_UploadData-main\CSV - larry1\CCTV-out-dt'
+
+save_path = r'C:\Users\phutadon\OneDrive\Desktop\Playground_UploadData-main\CSV - larry1\link'
+
+# สร้างรายการไฟล์ CSV ทั้งหมดในไดเรกทอรี
+csv_files = [f for f in os.listdir(dir_path) if f.endswith('.csv')]
+
+# อ่านและรวมข้อมูลจากไฟล์ CSV ทั้งหมด
+for file in csv_files:
+    file_path = os.path.join(dir_path, file)
+    df = pd.read_csv(file_path)
+    df_selected = df[['CAMERA_NAME', 'NVR RTSP MAIN']]
+    
+    # สร้างคอลัมน์ใหม่สำหรับ src_encoded
+    df_selected['NVR RTSP MAIN ENCODED'] = df_selected['NVR RTSP MAIN'].apply(lambda x: 'ffmpeg:' + x.replace("&", "%26"))
+    
+    # สร้างชื่อไฟล์ที่เซฟตามไฟล์ที่อ่านและต่อท้ายด้วย -link
+    txt_file_name = os.path.splitext(file)[0] + '-link.txt'
+    txt_file_path = os.path.join(save_path, txt_file_name)
+    
+    # บันทึกข้อมูลลงในไฟล์ txt
+    with open(txt_file_path, 'w') as txt_file:
+        for index, row in df_selected.iterrows():
+            txt_file.write(f"    {row['CAMERA_NAME']}: {row['NVR RTSP MAIN ENCODED']}\n")
+
+    # แสดงผล DataFrame ที่เลือก
+    print(df_selected)
