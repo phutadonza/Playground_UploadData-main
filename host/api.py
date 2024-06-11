3d39bb44137085a03076070410d6e1a7f2c6b619
Donutto <phutadol5627@gmail.com>
2024-06-11 18:02:24 +0700

    Update api.py

--- host/api.py
+++ host/api.py
@@ -1,2 +1,2 @@
-# API = 'a4pddFpUwhZ0We6ILSQcFWd1w0LiUoHqc8kFXKgrZY615fjLXJpvHpaZXldIj0QW'  #CCTV1 for test
-API = 'k3hqivYfUBAEE7BoL1745jQE4PGS0Vq8CL755gMen1tm1shTatHSkyilgi1YnGGa' #bkk larry
+API = 'a4pddFpUwhZ0We6ILSQcFWd1w0LiUoHqc8kFXKgrZY615fjLXJpvHpaZXldIj0QW'  #CCTV1 for test
+# API = 'k3hqivYfUBAEE7BoL1745jQE4PGS0Vq8CL755gMen1tm1shTatHSkyilgi1YnGGa' #bkk larry
